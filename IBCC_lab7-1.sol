9eadecfcbf1c929ae612bfe3fe1d09911999e7be
"git-prudhvi" <"prudhvirajcool2001@gmail.com">
2021-09-22 12:24:08 +0530

    lab7

---
+++ IBCC_lab7-1.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.24;
+contract lab7inlab {
+    uint money;
+    
+    function depositMoney(uint tempMoney) public{
+        money+=tempmoney;
+    }
+    
+    function withdrawMoney(uint tempMoney) public{
+        money-=tempMoney;
+    }
+    function checkMoney() public view return(uint tempMoney){
+        return moeny;
+    }
+    
+}
